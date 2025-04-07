36c47e7cded351ef1eb91b73e6215e3ed7516dad
jonas01101111 <jonas1kiubi@gmail.com>
2025-04-07 10:52:28 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppWeatherNow/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppWeatherNow
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppWeatherNow/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppWeatherNow
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
