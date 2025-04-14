2805212ee19962f7e74106f6f11d74d64a901617
jonas01101111 <jonas1kiubi@gmail.com>
2025-04-14 10:52:40 -0300

    charles interrompeu denovo

--- MauiAppWeatherNow/Services/DataService.cs
+++ MauiAppWeatherNow/Services/DataService.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MauiAppWeatherNow.Models;
 
 namespace MauiAppWeatherNow.Services
 {
     internal class DataService
     {
-       
+       public static async Task<Tempo?> GetPrevisao(string cidade) 
+        {
+            Tempo? t = null;
+            
+            string chave 
+        }
 
 }
 }
