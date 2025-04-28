88611684b9d7642d63bb5a81e23cf1469a804137
jonas01101111 <jonas1kiubi@gmail.com>
2025-04-28 11:37:09 -0300

    aula 15 51:12

--- MauiAppWeatherNow/Services/DataService.cs
+++ MauiAppWeatherNow/Services/DataService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MauiAppWeatherNow.Models;
+using Newtonsoft.Json.Linq;
 
 namespace MauiAppWeatherNow.Services
 {
@@ -12,8 +13,42 @@ namespace MauiAppWeatherNow.Services
        public static async Task<Tempo?> GetPrevisao(string cidade) 
         {
             Tempo? t = null;
-            
-            string chave 
+
+            string chave = "2b659183e24b6aa7f7a46de3bab4d57e";
+            string url = $"http://api.openweathermap.org/data/2.5/weather?" + 
+                         $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if (resp.IsSuccessStatusCode) 
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime rise = time.AddSeconds().ToLocaltime();
+                    DateTime set = time.AddSeconds().ToLocaltime();
+
+                    t = new()
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["long"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_min"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        sunrise = (string)rascunho["sys"]["sunrise"],
+                        sunset = (string)rascunho["sys"]["sunrise"],
+                        visibility= (int)rascunho["visibility"]
+                    };
+                }
+            }
+
+            return t;
         }
 
 }
